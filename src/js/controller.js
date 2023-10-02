9695e766698b4e6ec13fa11cfd60905eea628973
Manuel Henriquez <76463428+ManuelDHenriquez@users.noreply.github.com>
2023-10-02 00:16:50 -0600

    Fix navbar

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,18 @@ images.forEach((image) => {
     </a>
     `;
 });
+
+const checkbox = document.getElementById('checkbox');
+const navbar_container = document.getElementById('navbar_container');
+
+checkbox.addEventListener('change', () => {
+    // change theme of website
+    console.log('cambio de tema');
+    if (checkbox.checked) {
+        navbar_container.classList.add('white');
+        navbar_container.classList.remove('dark');
+    } else {    
+        navbar_container.classList.add('dark');
+        navbar_container.classList.remove('white');
+    }
+});
